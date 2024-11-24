92f670f2f2a9d3824d325c44e5451745f170101c
Andrey Luiz Pereira <andreylpereira@gmail.com>
2024-11-24 10:33:05 -0300

    [ADD] Dividindo a camada de Auth.

--- src/main/java/com/senai/destino/api/config/SecurityConstants.java
+++ src/main/java/com/senai/destino/api/config/SecurityConstants.java
@@ -2,6 +2,6 @@ package com.senai.destino.api.config;
 
 public class SecurityConstants {
 
-    public static final long JWT_EXPIRATION = 62000 * 60 * 24;
+    public static final long JWT_EXPIRATION = 62000 * 60 * 24; //Equivale a 1 dia
     public static final String JWT_SECRET = "uniSENAI-2024";
 }
--- src/main/java/com/senai/destino/api/controllers/AuthController.java
+++ src/main/java/com/senai/destino/api/controllers/AuthController.java
@@ -1,50 +1,26 @@
 package com.senai.destino.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.senai.destino.api.config.JWTGenerator;
 import com.senai.destino.api.dtos.AuthDTO;
 import com.senai.destino.api.dtos.AuthResponseDTO;
-import com.senai.destino.api.repositories.UsuarioRepository;
+import com.senai.destino.api.services.AuthService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-	private AuthenticationManager authenticationManager;
-	private UsuarioRepository usuarioRepository;
-	private PasswordEncoder passwordEncoder;
-	private JWTGenerator jwtGenerator;
-
 	@Autowired
-	public AuthController(AuthenticationManager authenticationManager, UsuarioRepository usuarioRepository,
-			PasswordEncoder passwordEncoder, JWTGenerator jwtGenerator) {
-		this.authenticationManager = authenticationManager;
-		this.usuarioRepository = usuarioRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.jwtGenerator = jwtGenerator;
-
-	}
+	private AuthService authService;	
 
 	@PostMapping("/login")
-	public ResponseEntity<AuthResponseDTO> login(@RequestBody AuthDTO authDTO) {
-		Authentication authentication = authenticationManager
-				.authenticate(new UsernamePasswordAuthenticationToken(authDTO.getNome(), authDTO.getSenha()));
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String token = jwtGenerator.generateToken(authentication);
-
-		return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.ACCEPTED);
+	public ResponseEntity<AuthResponseDTO> auth(@RequestBody AuthDTO authDTO) {
+		return authService.login(authDTO);
 	}
 
 }
